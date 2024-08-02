5dae6b4e38f2e0258a6dccce4439436a8bb2b55e
SanjanaRawal <sanjana.rawal2006@gmail.com>
2024-08-02 22:00:59 +0530

    added 1 more pattern

---
+++ patterns/13th.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout<<"Enter number : ";
+    cin>>n;
+
+//     for(int i=1 ; i<=n ; i++)
+//     {
+//         int val=i;
+//         for(int j=1 ; j<=i; j++)
+//         {
+//             cout<<val<<"\t";
+//             val--;
+//         }
+//         cout<<endl;
+//     }
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        for(int j=1 ; j<=i; j++)
+        {
+            cout<<i-j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
